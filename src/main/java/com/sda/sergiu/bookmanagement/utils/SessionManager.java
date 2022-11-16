976117dd5ca34cd41218b9f1e2c7a8cface0868e
Sergiu Marandescu <sergiu.marandescu@gmail.com>
2022-11-16 18:21:31 +0200

    BMS-15: Add hibernate configuration(pom and utils). DONE

--- src/main/java/com/sda/sergiu/bookmanagement/utils/SessionManager.java
+++ src/main/java/com/sda/sergiu/bookmanagement/utils/SessionManager.java
@@ -1,7 +1,4 @@
 package com.sda.sergiu.bookmanagement.utils;
-
-import com.sda.sergium.hibernate.ex1.model.Employee;
-import com.sda.sergium.hibernate.utils.AbstractSessionManager;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -23,7 +20,6 @@ public class SessionManager extends AbstractSessionManager {
     @Override
     protected void setAnnotatedClasses(Configuration configuration) {
         //Hibernate model will be added here
-        configuration.addAnnotatedClass(Employee.class);
     }
 }
 
